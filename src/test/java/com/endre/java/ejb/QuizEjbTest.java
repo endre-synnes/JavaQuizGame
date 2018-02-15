15d1701023187baf5b846f256e23d57715be5617
synend16 <synend16@student.westerdals.no>
2018-02-15 22:09:55 +0100

    Nesten fullført øving 6

--- src/test/java/com/endre/java/ejb/QuizEjbTest.java
+++ src/test/java/com/endre/java/ejb/QuizEjbTest.java
@@ -1,13 +1,15 @@
 package com.endre.java.ejb;
 
 import com.endre.java.entity.Quiz;
+import com.endre.java.entity.SubCategory;
 import org.junit.Test;
 
 import javax.ejb.EJB;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class QuizEjbTest extends EjbTestBase {
 
@@ -46,5 +48,65 @@ public class QuizEjbTest extends EjbTestBase {
 
         assertEquals(1, quizEjb.getQuizzes().size());
         assertEquals(question, quizEjb.getQuiz(quizId).getQuestion());
+    }
+
+
+    @Test
+    public void testNotEnoughQuizzes() {
+
+        long ctgId = createQuizzes("a", "b", "c");
+
+
+        try {
+            quizEjb.getRandomQuizzes(5, ctgId);
+            fail();
+        }catch (Exception e){
+            //exception (this should fail
+        }
+
+    }
+
+    private long createQuizzes(String... names){
+
+        long subId = createCtgAndSub("foo", "bar");
+
+        for (String n: names) {
+            quizEjb.createQuiz(subId,
+                    n,
+                    "yes",
+                    "no",
+                    "maybe",
+                    "noe idea",
+                    1);
+        }
+
+        return categoryEjb.getSubcategory(subId).getParent().getId();
+    }
+
+    @Test
+    public void testGetRandomQuizzes() {
+
+        long ctgId = createQuizzes("a", "b", "c");
+
+        Set<String> questions = new HashSet<>();
+
+        for (int i = 0; i < 50; i++) {
+
+            List<Quiz> quizzes = quizEjb.getRandomQuizzes(2, ctgId);
+            assertEquals(2, quizzes.size());
+
+            Quiz first = quizzes.get(0);
+            Quiz second = quizzes.get(1);
+
+            assertNotEquals(first.getQuestion(), second.getQuestion());
+
+            questions.add(first.getQuestion());
+            questions.add(second.getQuestion());
+        }
+
+        assertEquals(3, questions.size());
+        assertTrue(questions.contains("a"));
+        assertTrue(questions.contains("b"));
+        assertTrue(questions.contains("c"));
     }
 }
